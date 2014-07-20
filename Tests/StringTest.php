c1bfdd51d1ee6dbaaf3bc2a8fc53707ad23b62ee
Flug <flugv1@gmail.com>
2014-07-20 16:53:13 +0200

    add unittest for test classes

---
+++ Tests/StringTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace System\Test;
+
+use System\Integer;
+use System\String;
+
+class StringTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testInstance()
+	{
+		$stringValue = "Test";
+        $stringCompare = "Test Compare";
+        $stringConcact = "Test Contact";
+        $stringReplace = "Test Success";
+		$string = new String($stringValue);
+
+		$this->assertSame($stringValue, $string->__toString());
+        $this->assertTrue($string->Compare(new String($stringValue)));
+        $this->assertFalse($string->Compare(new String($stringCompare)));
+        $this->assertSame($stringValue.$stringConcact, $string->Concat(new String($stringConcact))->__toString());
+        $this->assertFalse($string->IsNullOrEmpty());
+        $this->assertSame($stringReplace, $string->Replace(new String($stringValue.$stringConcact), new String("Test Success") , null)->__toString());
+
+	}
+}
