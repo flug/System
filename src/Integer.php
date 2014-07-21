38a41ab87c108a72db827b80abd212b79a01316d
Mathias Strasser <mstrasser@universcine.com>
2014-07-21 20:13:02 +0200

    Add CompareTo method for Integer
    Compares this instance to a specified integer and returns an indication
    of their relative values

--- src/Integer.php
+++ src/Integer.php
@@ -2,23 +2,47 @@
 
 namespace System;
 
-use System\String;
+class Integer
+{
+    private $integer;
 
-class Integer extends String {
-
-    private $integer ;
-
-
-    public function __construct($integer){
-        if($integer !== (int) $integer)
-        {
-            $this->integer = (int) $integer;
+    public function __construct($integer)
+    {
+        if ($integer !== (int)$integer) {
+            $this->integer = (int)$integer;
         }
         $this->integer = $integer;
     }
 
-    public function __toValue()
+    public function CompareTo($value)
     {
-        return (int)$this->integer;
+        if (is_int($value)) {
+            if ($this->integer < $value) {
+                return -1;
+            }
+            if ($this->integer > $value) {
+                return 1;
+            }
+
+            return 0;
+        } else {
+            if (!$value) {
+                return 1;
+            }
+
+            /** @var \System\Integer $value */
+            if (__NAMESPACE__ . '\\Integer' === get_class($value)) {
+                if ($this->integer < $value->integer) {
+                    return -1;
+                }
+                if ($this->integer > $value->integer) {
+                    return 1;
+                }
+
+                return 0;
+            }
+
+            throw new \InvalidArgumentException('Arg_MustBeInt');
+        }
     }
 }
